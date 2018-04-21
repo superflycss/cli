5c2fc258586b03dc1f2b23940267d338faa583ea
Ole <ole.ersoy@gmail.com>
2018-04-20 19:55:44 -0500

    Added html test template

--- index.js
+++ index.js
@@ -42,23 +42,87 @@ const postcss_plugins =
     pc_font_magician, 
     autoprefixer, pc_reporter({
         clearMessages: true
-    })]; 
+    })];
 
-const generateNPMTemplate = (name) => {
-        return 
-            `{
-                "name": ${name},
-                "version": "1.0.0",
-                "description": "",
-                "main": "src/main/index.css",
-                "scripts": {
-                  "test": "echo \"Error: no test specified\" && exit 1"
-                },
-                "keywords": [],
-                "license": "ISC"
-              }  
-            `
+const generatePackageJson = (name) => {
+        const package = 
+        `
+{
+    "name": "${name}",
+    "version": "1.0.0",
+    "description": "",
+    "main": "src/main/index.css",
+    "scripts": {
+        "build": "sfc build:main:css",
+        "test": "sfc build:main:css && build:test:css && test:css"
+    },
+    "keywords": [],
+    "license": "ISC"
+}
+        `;
+            return package;
         }
+
+const generateTestHtml = (name) => {
+            const html =  
+            `
+<!doctype html>
+<html lang="en">
+<head>
+    <title>${name} Tests</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="${name} Tests">
+    <meta name="author" content="">
+    <!-- Font Awesome -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Test Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+</head>
+
+<body class="Body">
+    <header class="Header--column">
+        <h1 class="Display--fs400fw700 u-font-lato">${name} Test</h1>
+        <a class="Header_link"
+            href="https://github.com/superflycss/${name}/">Github</a>
+    </header>
+
+    <div class="Test">
+        <span class="Test_counter"></span>
+        <span class="Test_component">Grid</span>
+
+        <div class="Test_container">
+            <div class="Test_input">
+                <div class="Test_when"></div>
+                <div class="Test_description">When the
+                    <code>.Grid</code> container holds 2
+                    <code>.Grid-cell</code> instances.
+                </div>
+                <pre class="Test_markup">
+                    <code class="html">
+                        <div class="Grid">
+                            <!-- Use Nunjucks to keep markup DRY -->
+        {% for cell in ['1', '2'] %}
+                            <div class="Grid_cell">{{cell}}/2</div>
+        {% endfor %}
+                        </div>
+                    </code>
+                </pre>
+            </div>
+            <div class="Test_output">
+                <div class="Test_then"></div>
+                <div class="Test_description">Each grid cell occupies the
+                    same amount of space witin the grid container row.</div>
+            </div>
+        </div>
+    </body>
+</html>
+            `
+            return html;
+            }
+            
+
         
 
 
@@ -152,7 +216,8 @@ cli.
         mkdirp.sync(`${name}/${pli.src.test.html}`);
         mkdirp.sync(`${name}/${pli.src.main.js}`);
         mkdirp.sync(`${name}/${pli.src.test.js}`);
-        fs.writeFileSync(`./${name}/package.json`, generateNPMTemplate(name));
+        fs.writeFileSync(`./${name}/package.json`, generatePackageJson(name));
+        fs.writeFileSync(`./${name}/${pli.src.test.html}/index.html`, generateTestHtml(name));
     });
 
 cli.
