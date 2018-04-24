0271c7e7787bee6758f301c64b5eb7b047c6349f
Ole <ole.ersoy@gmail.com>
2018-04-23 20:12:30 -0500

    Moved generators to lib/generators directory

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const log = require('node-pretty-log');
 
 const fs = require('fs');
@@ -29,6 +28,11 @@ const highlight = require('highlight.js');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
+//GENERATORS 
+const generatePackageJson = require('./lib/generators/package-json');
+const generateTestHtml = require('./lib/generators/index-html');
+const generateGitignore = require('./lib/generators/gitignore');
+
 const postcss_plugins = 
    [pc_import, 
     pc_each, 
@@ -43,90 +47,7 @@ const postcss_plugins =
     autoprefixer, pc_reporter({
         clearMessages: true
     })];
-
-const generatePackageJson = (name) => {
-        const package = 
-        `
-{
-    "name": "${name}",
-    "version": "1.0.0",
-    "description": "",
-    "main": "src/main/index.css",
-    "scripts": {
-        "build": "sfc build:main:css",
-        "test": "sfc build:main:css && build:test:css && test:css"
-    },
-    "keywords": [],
-    "license": "ISC"
-}
-        `;
-            return package;
-        }
-
-const generateTestHtml = (name) => {
-            const html =  
-            `
-<!doctype html>
-<html lang="en">
-<head>
-    <title>${name} Tests</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="${name} Tests">
-    <meta name="author" content="">
-    <!-- Font Awesome -->
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Test Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="../css/index.css">
-</head>
-
-<body class="Body">
-    <header class="Header--column">
-        <h1 class="Display--fs400fw700 u-font-lato">${name} Test</h1>
-        <a class="Header_link"
-            href="https://github.com/superflycss/${name}/">Github</a>
-    </header>
-
-    <div class="Test">
-        <span class="Test_counter"></span>
-        <span class="Test_component">Grid</span>
-
-        <div class="Test_container">
-            <div class="Test_input">
-                <div class="Test_when"></div>
-                <div class="Test_description">When the
-                    <code>.Grid</code> container holds 2
-                    <code>.Grid-cell</code> instances.
-                </div>
-                <pre class="Test_markup">
-                    <code class="html">
-                        <div class="Grid">
-                            <!-- Use Nunjucks to keep markup DRY -->
-        {% for cell in ['1', '2'] %}
-                            <div class="Grid_cell">{{cell}}/2</div>
-        {% endfor %}
-                        </div>
-                    </code>
-                </pre>
-            </div>
-            <div class="Test_output">
-                <div class="Test_then"></div>
-                <div class="Test_description">Each grid cell occupies the
-                    same amount of space witin the grid container row.</div>
-            </div>
-        </div>
-    </body>
-</html>
-
-            `
-            return html;
-            }
-            
-
         
-
-
 function testCSS() {
     console.log(pli.SRC_TEST_HTML);
     globby([pli.SRC_TEST_HTML]).then((paths)=> {
@@ -218,8 +139,9 @@ cli.
         mkdirp.sync(`${name}/${pli.src.main.js}`);
         mkdirp.sync(`${name}/${pli.src.test.js}`);
         fs.writeFileSync(`./${name}/package.json`, generatePackageJson(name));
+        fs.writeFileSync(`./${name}/.gitignore`, generateGitignore());
         fs.writeFileSync(`./${name}/${pli.src.test.html}/index.html`, generateTestHtml(name));
-    });
+});
 
 cli.
 command('clean').
